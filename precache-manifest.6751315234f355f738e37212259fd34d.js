0e9ff89709605d720dce0a714fdd75f837ccb989
SiddhantKapadne <ksiddhant46@gmail.com>
2020-05-13 13:31:31 +0530

    Updates

---
+++ precache-manifest.6751315234f355f738e37212259fd34d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea062ffd245a07e489f2e13f93a31569",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "ef10bf83782f782a633f",
+    "url": "/Covid-19/static/css/main.5d4b2690.chunk.css"
+  },
+  {
+    "revision": "5ed4240309b88547e080",
+    "url": "/Covid-19/static/js/2.77ab4d5e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.77ab4d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef10bf83782f782a633f",
+    "url": "/Covid-19/static/js/main.77b66fff.chunk.js"
+  },
+  {
+    "revision": "d11cb2720ff7b0ecd208",
+    "url": "/Covid-19/static/js/runtime-main.9d903f23.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  }
+]);
